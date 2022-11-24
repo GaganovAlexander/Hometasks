c98869c4b6db6265d5f2b8eb23eefe87732977f0
GaganovAlexander <115991265+GaganovAlexander@users.noreply.github.com>
2022-11-24 12:01:19 +0300

    quick sort new vertion

--- Tests.py
+++ Tests.py
@@ -1,5 +1,5 @@
 from random import randrange
-import time
+from time import perf_counter
 from SomePreviousAlgorithms import *
 from BinaryInsertSort import *
 from merge_sort import *
@@ -11,13 +11,13 @@ NUM_OF_ELEMENTS = 10**4 + 3
 
 
 def check(sample: list[int], bild_in_function, *functions):
-    start_time = time.perf_counter()
+    start_time = perf_counter()
     bild_in_result = bild_in_function(sample)
-    print(f'Bild in function: \033[34m{time.perf_counter() - start_time} \033[0mSeconds')
+    print(f'Bild in function: \033[34m{perf_counter() - start_time} \033[0mSeconds')
     for func in functions:
-        start_time = time.perf_counter()
+        start_time = perf_counter()
         our_result = func(sample)
-        print(f'\033[0m{func.__name__}: \033[34m{round(time.perf_counter() - start_time, 7)} \033[0mSeconds')
+        print(f'\033[0m{func.__name__}: \033[34m{round(perf_counter() - start_time, 7)} \033[0mSeconds')
         if bild_in_result == our_result:
             print(f'\033[32mTrue\033[0m')
         else:
@@ -28,16 +28,16 @@ def check_with_method(sample: list[int], variations: list[int], func):
     total_time_of = 0
 
     for i in variations:
-        start_time = time.perf_counter()
+        start_time = perf_counter()
         try:
             bild_in_result = sample.index(i)
         except:
             bild_in_result = None
-        total_time_bi += time.perf_counter() - start_time
+        total_time_bi += perf_counter() - start_time
 
-        start_time = time.perf_counter()
+        start_time = perf_counter()
         our_result = func(sample, i)
-        total_time_of += time.perf_counter() - start_time
+        total_time_of += perf_counter() - start_time
 
         if bild_in_result != our_result:
             print(our_result, bild_in_result)
@@ -46,4 +46,4 @@ def check_with_method(sample: list[int], variations: list[int], func):
 
 random_sample = [randrange(-MAX_RANGE, MAX_RANGE) for _ in range(NUM_OF_ELEMENTS)]
 #check_with_method(sorted(random_sample), [randrange(-1000, 1000) for _ in range(100)], binary_search)
-check(random_sample, sorted, merge_sort, binary_insert_sort, merge_sort_without_rec, quick_sort)
+check(random_sample, sorted, merge_sort, binary_insert_sort, merge_sort_without_rec, quick_sort, quick_sort_one_for)
--- quick_sort.py
+++ quick_sort.py
@@ -5,5 +5,25 @@ def quick_sort(sample: list[int]) -> list[int]:
         leasser = [i for i in sample if i < sample[0]]
         equal = [i for i in sample if i == sample[0]]
         greater = [i for i in sample if i > sample[0]]
+        return quick_sort(leasser) + equal + quick_sort(greater)
 
+def quick_sort_one_for(sample: list[int]) -> list[int]:
+    if len(sample) <= 1:
+        return sample
+    if len(sample) == 2:
+        if sample[0] > sample[1]:
+            return sample[::-1]
+        else:
+            return sample
+    else:
+        leasser = []
+        equal = []
+        greater = []
+        for i in sample:
+            if i < sample[0]:
+                leasser.append(i)
+            elif i > sample[0]:
+                greater.append(i)
+            else:
+                equal.append(i)
         return quick_sort(leasser) + equal + quick_sort(greater)
