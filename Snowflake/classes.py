c3ae43e93525aa3da012e69e07234c62f725aa09
GaganovAlexander <115991265+GaganovAlexander@users.noreply.github.com>
2022-11-26 21:05:50 +0300

    Snowflake color and color change added

--- Snowflake/classes.py
+++ Snowflake/classes.py
@@ -1,27 +1,27 @@
-class Student:
-    def __init__(self, name: str, age: int) -> None:
-        self.__name = name 
-        self.__age = age
-    
-    def get_info(self) -> None:
-        print(f'{self.__name}: {self.__age} years old')
-
-
 class Snowflake:
-    def __init__(self, size: int, thickness: int) -> None:
-        '''Size and thickness must be an odd numbers'''
+    def __init__(self, size: int, thickness: int, color: str = 'turquoise') -> None:
+        '''Size and thickness must be an odd numbers. Available colors are:\
+        black, red, green, yellow, blue, purple and turquoise'''
         self.__thickness = thickness
         self.__size = size
+        self.__colors = {'black': 30,
+                         'red': 31,
+                         'green': 32,
+                         'yellow': 33,
+                         'blue': 34,
+                         'purple': 35,
+                         'turquoise': 36}
+        self.__color = self.__colors[color.lower()]
         self.__creating_structure()
         
     def __creating_structure(self) -> None:
         self.__structure = [[' ' for _ in range(self.__size)] for _ in range(self.__size)]
-        self.__structure[self.__size//2 - (self.__thickness-1)//2:self.__size//2 + (self.__thickness-1)//2 + 1] = [[f'\033[34m*' for _ in range(self.__size)] for _ in range(self.__thickness)]
+        self.__structure[self.__size//2 - (self.__thickness-1)//2:self.__size//2 + (self.__thickness-1)//2 + 1] = [[f'\033[{self.__color}m*' for _ in range(self.__size)] for _ in range(self.__thickness)]
         for j in range(self.__size):
-            self.__structure[j][self.__size//2 - (self.__thickness-1)//2:self.__size//2 + (self.__thickness-1)//2 + 1] = [f'\033[34m*' for _ in range(self.__thickness)]
+            self.__structure[j][self.__size//2 - (self.__thickness-1)//2:self.__size//2 + (self.__thickness-1)//2 + 1] = [f'\033[{self.__color}m*' for _ in range(self.__thickness)]
             for i in range(self.__size):
                 if abs(i - j) <= (self.__thickness-1)//2 or self.__size - 1 - (self.__thickness-1)//2 <= i + j <= self.__size - 1 + (self.__thickness-1)//2:
-                    self.__structure[j][i] = f'\033[34m*'  
+                    self.__structure[j][i] = f'\033[{self.__color}m*'  
         self.__structure[self.__size-1][self.__size-1] += f'\033[0m'     
 
     def get_view(self) -> None:
@@ -30,11 +30,19 @@ class Snowflake:
             print(' '.join(i))
     
     def add_thickness(self, added_thickness: int) -> None:
-        '''Adds added_thickness to the shoflake thickness. added_thickness must be even; if it's negative - will reduse thickness'''
+        '''Adds added_thickness to the shoflake thickness. added_thickness must be even;\
+        if it's negative - will reduse thickness'''
         self.__thickness += added_thickness
         self.__creating_structure()
     
     def resize(self, added_size: int) -> None:
-        '''Adds added_size to the shoflake size. added_size must be even; if it's negative - will reduse size'''
+        '''Adds added_size to the shoflake size. added_size must be even;\
+        if it's negative - will reduse size'''
         self.__size += added_size
+        self.__creating_structure()
+    
+    def change_color(self, color: str = 'turquoise'):
+        '''Changes snowflake's color, if no arguments entered - will change color to default turquoise.\
+        Available colors are: black, red, green, yellow, blue, purple and turquoise'''
+        self.__color = self.__colors[color.lower()]
         self.__creating_structure()
--- Snowflake/first_class_use.py
+++ Snowflake/first_class_use.py
@@ -2,10 +2,11 @@ from classes import Snowflake
 
 
 def main():
-    s1 = Snowflake(21, 5)
+    s1 = Snowflake(21, 5, 'red')
     s1.get_view()
     s1.resize(-2)
     s1.add_thickness(-2)
+    s1.change_color()
     s1.get_view()
 
 if __name__ == '__main__':
